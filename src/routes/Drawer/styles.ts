79478402ddb1ee990f62d998f1ad0ad030678a34
Gabriel Dissotti <gabrieldnrodrigues@gmail.com>
2020-11-24 21:42:43 -0300

    feat(responsive): added limit in width to use app on desktops

--- src/routes/Drawer/styles.ts
+++ src/routes/Drawer/styles.ts
@@ -1,8 +1,21 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
+import { isWebPlatform } from '../../constants';
+import { pixels } from '../../functions';
 import colors from '../../theme/colors';
 
 export const Container = styled.View`
   background-color: ${colors.primaryDefault};
 
   overflow: hidden;
+
+  ${isWebPlatform &&
+  css`
+    max-width: ${pixels(600)};
+    margin: auto;
+    position: relative;
+    height: 100%;
+    width: 100%;
+    border: 1px solid #f1f1f1;
+    border-radius: ${pixels(16)};
+  `}
 `;
